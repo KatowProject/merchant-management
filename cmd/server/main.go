b6009a04f46e58d3cd70fa1c908a92bf57a2c250
KatowProject <naufalfaqih443@gmail.com>
2025-04-21 10:46:20 +0700

    Add dotenv support and configure server port from environment variable

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load("configs/.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
@@ -18,5 +25,10 @@ func main() {
 		})
 	})
 
-	r.Run(":8080") // listen and serve on
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+
+	r.Run(":" + PORT)
 }
